b0b2b77b460069e7953526f5fcccc02d20cd51b3
Jamshid Ismailov <jamshid@Jamshids-MacBook-Pro.local>
2019-06-28 16:21:57 +0500

    Updates

---
+++ precache-manifest.437559dcb0d15c1fb338585e1600d876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cefe7a7a53dc42d304308b96c987fa",
+    "url": "Jamshid-din/todolist.github.io/index.html"
+  },
+  {
+    "revision": "4b6af080f20b655df80b",
+    "url": "Jamshid-din/todolist.github.io/static/css/main.8e2fea03.chunk.css"
+  },
+  {
+    "revision": "5e161533e3fe514cd118",
+    "url": "Jamshid-din/todolist.github.io/static/js/2.85ab41d6.chunk.js"
+  },
+  {
+    "revision": "4b6af080f20b655df80b",
+    "url": "Jamshid-din/todolist.github.io/static/js/main.d70bb745.chunk.js"
+  },
+  {
+    "revision": "abe8723b8fc170640a07",
+    "url": "Jamshid-din/todolist.github.io/static/js/runtime~main.bda67ca7.js"
+  }
+]);
